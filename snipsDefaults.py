8cf0c5e8f3fd625f47f80ac1c07eb5f196d555e9
acolytec3 <konjou@gmail.com>
2018-09-01 15:45:27 -0400

    Delete snipsDefaults.py
    Deprecated now that snips-app-server is intended

--- snipsDefaults.py
+++
@@ -1,6 +0,0 @@
-
-# Twitter keys
-consumerkey ='' #Put your consumer key provided by the Twitter dev console here
-consumersecret ='' #Put your consumer secret provided by the Twitter dev console here
-appkey= '' #Put your app key provided by the Twitter dev console here
-appsecret = '' #Put your app secret provided by the Twitter dev console here
